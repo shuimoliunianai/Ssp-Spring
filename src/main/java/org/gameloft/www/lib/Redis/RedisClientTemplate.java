68f1c565b6bf5c66620319f5eec9186c5bfeaf1b
shuimoliunianai <649591475@qq.com>
2016-11-23 17:34:43 +0800

    Update RedisClientTemplate.java

--- src/main/java/org/gameloft/www/lib/Redis/RedisClientTemplate.java
+++ src/main/java/org/gameloft/www/lib/Redis/RedisClientTemplate.java
@@ -1,116 +1,121 @@
 package org.gameloft.www.lib.Redis;
 
-import com.mchange.lang.LongUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import redis.clients.jedis.ShardedJedis;
 import redis.clients.jedis.ShardedJedisPipeline;
 
 import javax.annotation.PreDestroy;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Set;
 
 /**
- * Created by apple on 16/11/3.
+ * Stats Handle
+ * Created by ningzhen.ge on 2016/11/4.
  */
 @Component
 public class RedisClientTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(RedisClientTemplate.class);
 
+    /**
+     * pools
+     */
     @Autowired
-    private RedisDataSource redisDataSource;
+    private redisDataSourceImpl redisDataSource;
+
+    /**
+     * 批量处理客户端
+     */
+    private static ShardedJedisPipeline shardedJedisPipeline;
 
     /**
      * Logger
      */
-    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(RedisClientTemplate.class);
+    private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(RedisClientTemplate.class);
 
     /**
-     *
+     * @param key   。
+     * @param field 。
+     * @param value 。
+     * @return boolean 。
      */
-    private ShardedJedisPipeline shardedJedisPipeline;
-
-    /**
-     * 设置单个值
-     *
-     * @param key
-     * @param value
-     * @return
-     */
-    public  String set(String key, String value) {
-        String result = null;
-
-        ShardedJedis shardedJedis = redisDataSource.getRedisClient();
-        if (shardedJedis == null) {
-            return null;
-        }
-        try {
-            result = shardedJedis.set(key, value);
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
-        return result;
-    }
-
-    /**
-     * 获取单个值
-     *
-     * @param key
-     * @return
-     */
-    public  String get(String key) {
-        String result = null;
-        ShardedJedis shardedJedis = redisDataSource.getRedisClient();
-        if (shardedJedis == null) {
-            return result;
-        }
-
-        try {
-            result = shardedJedis.get(key);
-
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
-        return result;
-    }
-
-    /**
-     * 批量操作
-     */
-    public Boolean CallPipedOption(String key,String value ,long time)
-    {
-        if (StringUtils.isEmpty(key) || StringUtils.isEmpty(value))
-        {
+    public Boolean hset(final String key, final String field, final String value) {
+        if (StringUtils.isEmpty(key) || StringUtils.isEmpty(field) || StringUtils.isEmpty(value)) {
             return false;
         }
-        ShardedJedisPipeline shardedJedisPipeline= redisDataSource.getRedisClient().pipelined();
-        if (shardedJedisPipeline == null)
-        {
+        ShardedJedis shardedJedis = redisDataSource.getRedisClient();
+        if (shardedJedis == null) {
             return false;
         }
-        try{
-            shardedJedisPipeline.hincrBy(key,value,time);
-        }catch (Exception e)
-        {
-            logger.error(e.getMessage());
+        try {
+            shardedJedis.hset(key, field, value);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
             return false;
         }
         return true;
     }
 
-    @PreDestroy
-    public void destroyMethod() throws Exception {
-        try {
-            shardedJedisPipeline.syncAndReturnAll();
-        }catch (Exception e)
-        {
-            logger.error(e.getMessage());
+    /**
+     * @param key   。
+     * @param field 。
+     * @return boolean 。
+     */
+    public String hget(final String key, final String field) {
+        if (StringUtils.isEmpty(key) || StringUtils.isEmpty(field)) {
+            return null;
         }
-        System.out.println("destroyMethod 被执行");
+        ShardedJedis shardedJedis = redisDataSource.getRedisClient();
+        if (shardedJedis == null) {
+            return null;
+        }
+        try {
+            String response = shardedJedis.hget(key, field);
+            return response;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        } finally {
+            destroy();
+        }
+        return null;
     }
+
+    /**
+     * @param hash      。
+     * @param arguments 。
+     * @return true/false
+     */
+    public synchronized Boolean addHincrByToPipline(String hash, Set<String> arguments) {
+        if (StringUtils.isEmpty(hash) || arguments.isEmpty()) {
+            return false;
+        }
+        try {
+            ShardedJedis shardedJedis = redisDataSource.getRedisClient();
+            if (shardedJedis == null) {
+                return false;
+            }
+            if (shardedJedisPipeline == null) {
+                shardedJedisPipeline = shardedJedis.pipelined();
+            }
+            for (String argument : arguments) {
+                shardedJedisPipeline.hincrBy(hash, argument, 1);
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 当回收这个Bean时，执行先前的异步写入操作
+     */
+    @PreDestroy
+    public void destroy() {
+        if (shardedJedisPipeline != null) {
+            shardedJedisPipeline.syncAndReturnAll();
+        }
+    }
+
+
 }
